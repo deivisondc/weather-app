4c9fce116548b82a295cf537ef6aad257109fda2
Deivison Cardoso <deivisondc@gmail.com>
2019-09-11 22:04:29 -0300

    wind direction formatted

--- src/components/view/weather/WeatherDetail.vue
+++ src/components/view/weather/WeatherDetail.vue
@@ -56,7 +56,7 @@
           <p>UV index</p>
         </div>
         <div class="additional-info-values">
-          <p>West {{weather.wind.speed}}m/s</p>
+          <p>{{windDegressFormated}} {{weather.wind.speed}}m/s</p>
           <p v-if="weather.visibility">{{weather.visibility / 1000}} km</p>
           <p>{{weather.uvi.value}}</p>
         </div>
@@ -79,6 +79,30 @@ export default {
   },
   computed: {
     ...mapGetters('weather', ['weather', 'forecast']),
+
+    windDegressFormated() {
+      if (this.weather.wind.deg) {
+        const deg = this.weather.wind.deg;
+        if (deg > 11.25 && deg <= 33.75) return 'NNE'
+        if (deg > 33.75 && deg <= 56.25) return 'NE'
+        if (deg > 56.25 && deg <= 78.75) return 'ENE'
+        if (deg > 78.75 && deg <= 101.25) return 'E'
+        if (deg > 101.25 && deg <= 123.75) return 'ESE'
+        if (deg > 123.75 && deg <= 146.25) return 'SE'
+        if (deg > 146.25 && deg <= 168.75) return 'SSE'
+        if (deg > 168.75 && deg <= 191.25) return 'S'
+        if (deg > 191.25 && deg <= 213.75) return 'SSW'
+        if (deg > 213.75 && deg <= 236.25) return 'SW'
+        if (deg > 236.25 && deg <= 258.75) return 'WSW'
+        if (deg > 258.75 && deg <= 281.25) return 'W'
+        if (deg > 281.25 && deg <= 303.75) return 'WNW'
+        if (deg > 303.75 && deg <= 326.25) return 'NW'
+        if (deg > 326.25 && deg <= 348.75) return 'NNW'
+
+        return 'N';
+      }
+    },
+
     weatherIconUrl() {
       return process.env.VUE_APP_OPENWEATHER_BASE_ICON_URL;
     },
