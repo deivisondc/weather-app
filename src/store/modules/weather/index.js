93cbaadedec10cd50afaa0807747b53c5bbae7d4
Deivison Cardoso <deivisondc@gmail.com>
2019-09-15 18:26:44 -0300

    filtering forecast list (getting the max / min temp per day)

--- src/store/modules/weather/index.js
+++ src/store/modules/weather/index.js
@@ -1,4 +1,5 @@
-import axios from "@/services/axios";
+import Vue from 'vue';
+import axios from '@/services/axios';
 
 export default {
   namespaced: true,
@@ -26,41 +27,63 @@ export default {
     async fetchWeatherFromCity({ commit }, city) {
       city = {
         id: 123,
-        name: 'Bauru'
+        name: 'Bauru',
       };
 
       const params = {
         q: city.name,
         APPID: process.env.VUE_APP_OPENWEATHER_API_KEY,
         lang: 'pt',
-        units: 'metric'
-      }
+        units: 'metric',
+      };
 
       const forecast = await axios.get('forecast', { params }).then(res => res.data);
       const weather = await axios.get('weather', { params })
-        .then(async res => {
-
+        .then(async (res) => {
           params.lat = forecast.city.coord.lat;
           params.lon = forecast.city.coord.lon;
 
-          res.data.uvi = (await axios.get('uvi', { params })).data
+          res.data.uvi = (await axios.get('uvi', { params })).data;
           return res.data;
         });
 
-      console.log('vuex weather', weather)
-      console.log('vuex forecast', forecast)
+      // console.log('vuex weather', weather)
+      // console.log('vuex forecast', forecast)
+
+      const forecastListFiltered = {};
+      if (forecast.list) {
+        forecast.list.forEach((element) => {
+          const weekday = Vue.moment.unix(element.dt).format('dddd');
+
+          if (!forecastListFiltered[weekday]) {
+            forecastListFiltered[weekday] = element;
+          } else {
+            if (forecastListFiltered[weekday].main.temp_min > element.main.temp_min) {
+              forecastListFiltered[weekday].main.temp_min = element.main.temp_min;
+            }
+            if (forecastListFiltered[weekday].main.temp_max < element.main.temp_max) {
+              forecastListFiltered[weekday].main.temp_max = element.main.temp_max;
+            }
+          }
+        });
+      }
+
+      forecast.list = [];
+      Object.keys(forecastListFiltered).forEach((key) => {
+        forecast.list.push(forecastListFiltered[key]);
+      });
 
       commit('fetchWeatherData', weather);
       commit('fetchForecastData', forecast);
-    }
+    },
   },
 
   mutations: {
     fetchWeatherData(state, payload) {
-      state.weather = payload
+      state.weather = payload;
     },
     fetchForecastData(state, payload) {
-      state.forecast = payload
-    }
+      state.forecast = payload;
+    },
   },
-}
+};
