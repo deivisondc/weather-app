30214771e16f538c05bc65be55bf6c97e7d7e916
Deivison Cardoso <deivisondc@gmail.com>
2019-09-17 20:56:57 -0300

    changing container color (trying to fix icon contrast)

--- src/components/weatherDetails/ForecastContainer.vue
+++ src/components/weatherDetails/ForecastContainer.vue
@@ -19,7 +19,7 @@ export default {
 <style lang="scss" scoped>
 
   .weather-details-container {
-    background-color: #F9F9F9;
+    background-color: #F6F6F6;
     margin: 0 20px;
     border-radius: 10px;
     padding: 10px;
