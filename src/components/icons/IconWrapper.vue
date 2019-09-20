153aea79d56834c5f36d83e6290692d2d50e95f7
Deivison Cardoso <deivisondc@gmail.com>
2019-09-20 20:15:09 -0300

    wrapping all icons into an index.js file

--- src/components/icons/IconWrapper.vue
+++ src/components/icons/IconWrapper.vue
@@ -4,41 +4,11 @@
 </template>
 
 <script>
-import ClearNightIcon from '@/components/icons/ClearNightIcon';
-import LightningIcon from '@/components/icons/LightningIcon';
-import MostlyCloudyIcon from '@/components/icons/MostlyCloudyIcon';
-import MostlyCloudyNightIcon from '@/components/icons/MostlyCloudyNightIcon';
-import NightShowersIcon from '@/components/icons/NightShowersIcon';
-import PartlyCloudyIcon from '@/components/icons/PartlyCloudyIcon';
-import PartlyCloudyNightIcon from '@/components/icons/PartlyCloudyNightIcon';
-import RainyIcon from '@/components/icons/RainyIcon';
-import ShowersIcon from '@/components/icons/ShowersIcon';
-import SnowShowersIcon from '@/components/icons/SnowShowersIcon';
-import SnowyIcon from '@/components/icons/SnowyIcon';
-import SunnyIcon from '@/components/icons/SunnyIcon';
-import SunnyShowersIcon from '@/components/icons/SunnyShowersIcon';
-import ThunderShowersIcon from '@/components/icons/ThunderShowersIcon';
-import WindyIcon from '@/components/icons/WindyIcon';
-import WindySunnyIcon from '@/components/icons/WindySunnyIcon';
+import icons from '@/components/icons';
 
 export default {
   components: {
-    ClearNightIcon,
-    LightningIcon,
-    MostlyCloudyIcon,
-    MostlyCloudyNightIcon,
-    NightShowersIcon,
-    PartlyCloudyIcon,
-    PartlyCloudyNightIcon,
-    RainyIcon,
-    ShowersIcon,
-    SnowShowersIcon,
-    SnowyIcon,
-    SunnyIcon,
-    SunnyShowersIcon,
-    ThunderShowersIcon,
-    WindyIcon,
-    WindySunnyIcon,
+    ...icons
   },
   props: {
     iconName: ''
